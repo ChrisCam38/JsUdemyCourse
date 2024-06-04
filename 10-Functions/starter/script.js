4253621cbafc1d112a02653d86c09d72f96100ea
Christian Tilano <kristiantilano@gmail.com>
2024-06-04 09:31:55 -0500

    133. Functions returning functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,8 +1,39 @@
 'use strict';
 
 /////////////////////////////////////////////////////
-//132. Functions accepting callback functions
+//133. Functions returning functions
 
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greetHey = greet('Hey man');
+greetHey('Christian');
+greetHey('Carlos');
+
+//Doing the same but in 1 line
+greet('Hi!')('Lina');
+
+//Using arrow functions to write the same code
+const greet2 = greeting2 => {
+  return name => {
+    console.log(`${greeting2} ${name}`);
+  };
+};
+
+greet2('Hello')('Mayer');
+
+//An even more concise way of doing it
+const greetArr = greetingArr => nameArr =>
+  console.log(`${greetingArr} ${nameArr}`); //Arrow functions of 1 line dont need paratheses and return expression
+
+greetArr('Hola')('Pedro');
+
+/////////////////////////////////////////////////////
+//132. Functions accepting callback functions
+/*
 const oneWord = function (str) {
   return str.replace(/ /g, '').toLowerCase();
 };
@@ -33,6 +64,7 @@ const high5 = function () {
 document.body.addEventListener('click', high5);
 ['Lina', 'Laura', 'Camila'].forEach(high5);
 // Note: Callback functions enable abstraction.
+*/
 /*
     By using callback functions, we can hide the specifics of a particular problem,
     allowing us to focus on more abstract and general issues. 
@@ -50,7 +82,7 @@ document.body.addEventListener('click', high5);
   -Functions are just another type of object
 
 
-  With function we can:
+  With functions we can:
   -Store functions in variables or properties
   -pass functions as arguments of other functions
   -Return functions from functions
@@ -94,7 +126,7 @@ const checkIn = function (flightNum, passenger) {
 };
 
 checkIn(flight, christian);
-//Prints 'LH234' the same value contain in the flight variable
+//Prints 'LH234' the same value contained in the flight variable
 console.log(flight);
 //Overwrites the value of the object, because only copies the memory reference that points to the same object -> christian
 console.log(christian);
