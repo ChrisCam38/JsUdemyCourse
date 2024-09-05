80e88316aa670f76346fef0b171b5e4705135ab3
Christian Tilano <kristiantilano@gmail.com>
2024-09-05 00:09:07 -0500

    150. Data transformation: map, filter, reduce

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,15 +61,26 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-
 //Receives an array of movements and displays that data
-const displayMovements = function(movements){
-  movements.forEach(function(mov, i){
-    
-  })
-}
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
 
-displayMovements(account1.movements)
+  movements.forEach(function (mov, i) {
+    //Using a ternary operator to identify wheter is a deposit or withdrawal
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+          <div class="movements__value">${mov} €</div>
+        </div>
+        `;
+    containerMovements.insertAdjacentHTML('afterbegin', html); //Method that add the HTML in the container Movements
+  });
+};
+
+displayMovements(account1.movements);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -84,8 +95,81 @@ const currencies = new Map([
 //const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
-//146. forEach with maps and sets
+//150. Data transformations: map, filter, reduce
 
+//MAP
+/* Returns a new array containing the results of applying an
+operation on all original array methods*/
+
+//FILTER
+/*Retuns an array containing the array elements that  passed
+a specified test condition */
+
+//REDUCE
+
+/*Reduce all the elements of an array into one element (eg: adding all the elements in the array) */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////
+// 149. Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+let dogsJulia = [3, 5, 2, 12, 7];
+let dogsKate = [4, 1, 15, 8, 3];
+
+const checkDogs = function (dogsJulia, dogsKate) {
+  let dogsJuliaProper = dogsJulia.slice(1, -2);
+  console.log(dogsJuliaProper);
+  let allDogs = dogsJuliaProper.concat(dogsKate);
+  console.log(allDogs);
+  allDogs.forEach(function (dogAge, i, arr) {
+    dogAge >= 3
+      ? console.log(
+          `Dog number ${i + 1} is an adult and is ${dogAge} years old `
+        )
+      : console.log(`Dog number ${i + 1} is still a puppy`);
+  });
+};
+
+checkDogs(dogsJulia, dogsKate);
+
+/////////////////////////////////////////////////
+//146. forEach with maps and sets
+/*
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
@@ -103,7 +187,7 @@ console.log(curreciesUnique);
 curreciesUnique.forEach(function (value, _, map) {
   console.log(`${value}: ${_}`); //the second parameter is the same of the first one
 });
-
+*/
 /////////////////////////////////////////////////
 //145. Looping Arrays: forEach
 /*
